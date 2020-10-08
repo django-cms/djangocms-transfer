4938f80de27117fd9bcb201948de2c8d532a6069
Arne Brodowski <mail@arnebrodowski.de>
2020-10-08 10:27:21 +0200

    make isort happy

--- djangocms_transfer/forms.py
+++ djangocms_transfer/forms.py
@@ -3,8 +3,8 @@ import json
 from django import forms
 from django.conf import settings
 from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
 from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 
 from cms.models import CMSPlugin, Page, Placeholder
 
