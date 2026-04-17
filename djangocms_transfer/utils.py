476bfef9a3e9c182ad8c7a1f2527298e0d76747b
Vinit Kumar <vinit@django-cms.org>
2026-04-17 09:54:58 +0530

    chore: upgrade code to be python 3.10 plus only

--- djangocms_transfer/utils.py
+++ djangocms_transfer/utils.py
@@ -4,7 +4,7 @@ from cms.models import CMSPlugin
 from cms.plugin_pool import plugin_pool
 
 
-@lru_cache()
+@lru_cache
 def get_local_fields(model):
     opts = model._meta.concrete_model._meta
     fields = opts.local_fields
@@ -15,19 +15,19 @@ def get_local_fields(model):
     ]
 
 
-@lru_cache()
+@lru_cache
 def get_related_fields(model):
     opts = model._meta.concrete_model._meta
     fields = opts.local_fields + list(opts.many_to_many)
     return [field.name for field in fields if field.is_relation]
 
 
-@lru_cache()
+@lru_cache
 def get_plugin_class(plugin_type):
     return plugin_pool.get_plugin(plugin_type)
 
 
-@lru_cache()
+@lru_cache
 def get_plugin_fields(plugin_type):
     klass = get_plugin_class(plugin_type)
     if klass.model is CMSPlugin:
@@ -37,6 +37,6 @@ def get_plugin_fields(plugin_type):
     return [field.name for field in fields]
 
 
-@lru_cache()
+@lru_cache
 def get_plugin_model(plugin_type):
     return get_plugin_class(plugin_type).model
